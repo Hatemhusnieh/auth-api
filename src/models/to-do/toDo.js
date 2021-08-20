fac3f957483acc2eaf5c9354d9a77a6b3f929a5b
hatem <hatemhtm7@gmail.com>
2021-08-20 20:28:57 +0300

    lab 34

--- src/models/to-do/toDo.js
+++ src/models/to-do/toDo.js
@@ -6,6 +6,7 @@ const tpDoSchema = mongoose.Schema({
   assignee: { type: String, required: true },
   text: { type: String, required: true },
   complete: { type: Boolean },
+  difficulty: String,
 });
 
 const tpDoModel = mongoose.model('todo', tpDoSchema);
